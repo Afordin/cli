97d1153365ebe3ed357a84d148f6d0c73bcce9b3
Juan Pablo Barrera <juanpablo.is@outlook.com>
2023-11-19 22:22:45 -0500

    Demo CLI to generate templates

---
+++ packages/create-afordin/src/TEMPLATES.ts
@@ -0,0 +1,80 @@
+/**
+ * next-tailwind -> next/tailwind
+ * next-unocss -> next/unocss
+ *
+ * vite-react-unocss -> vite/react/unocss
+ * vite-react-tailwind -> vite/react/tailwind
+ * vite-react_ts-tailwind -> vite/react_ts/tailwind
+ */
+
+const allDesigns = [
+  {
+    value: 'tailwind',
+    title: 'TailwindCSS',
+  },
+  {
+    value: 'unocss',
+    title: 'UnoCSS',
+  },
+]
+
+export const TEMPLATES = [
+  {
+    value: 'vite',
+    title: 'Vite',
+    variant: [
+      {
+        value: 'react',
+        title: 'ReactJS',
+        design: allDesigns,
+      },
+      {
+        value: 'react_ts',
+        title: 'ReactJS con TypeScript',
+        design: allDesigns,
+      },
+      {
+        value: 'vue',
+        title: 'VueJS',
+        design: allDesigns,
+      },
+      {
+        value: 'vue_ts',
+        title: 'VueJS con TypeScript',
+        design: allDesigns,
+      },
+    ],
+  },
+  {
+    value: 'next',
+    title: 'NextJS',
+    design: allDesigns,
+  },
+]
+
+export function getAllTemplates (item: any[], parentKey: string | undefined, defaultAcc: Array<string>): Array<string> {
+  return item.reduce((acc, i) => {
+    const newKey = parentKey ? [parentKey, i.value].join('-') : i.value
+
+    if (i.variant) {
+      return getAllTemplates(i.variant, newKey, acc)
+    }
+
+    if (i.design) {
+      return getAllTemplates(i.design, newKey, acc)
+    }
+
+    return [...acc, newKey]
+  }, defaultAcc)
+}
+
+export function getPathTemplate (results: Record<'framework' | 'variant' | 'design', string>): string {
+  return ['framework', 'variant', 'design'].reduce((acc, item) => {
+    const value = results[item as keyof typeof results]
+    if (value) {
+      return acc + value + '/'
+    }
+
+    return acc
+  }, '')
+}
---
+++ packages/create-afordin/src/utils.ts
@@ -0,0 +1,182 @@
+import path, {resolve} from 'node:path'
+import fs from 'node:fs'
+
+import {DEFAULT_FOLDER} from './const'
+import {fileURLToPath} from 'node:url'
+
+export function args () {
+  return process.argv.slice(2).reduce(
+    (acc, item, i, arr) => {
+      const isFlag = item.startsWith('--')
+
+      if (isFlag) {
+        const flagValue = arr.splice(i + 1, 1).shift()
+
+        const flag = item.slice(2)
+        acc[flag] = flagValue
+      } else {
+        acc._.push(item)
+      }
+
+      return acc
+    },
+    {_: []} as Record<string, any>,
+  )
+}
+
+export async function catchify (promise: Promise<any>) {
+  return await promise.then(d => [null, d]).catch(e => [e, null])
+}
+
+export function targetDirectory (targetDirectory: string | undefined): [string, boolean] {
+  const directory = targetDirectory?.trim().replace(/\/+$/g, '')
+  return [directory || DEFAULT_FOLDER, !Boolean(directory)]
+}
+
+export function getProjectName (directory: string) {
+  return directory === '.' ? path.basename(path.resolve()) : directory
+}
+
+export function isEmptyDirectory (targetDir: string) {
+  return !fs.existsSync(targetDir) || isEmpty(targetDir)
+}
+
+export function isEmpty (path: string) {
+  const files = fs.readdirSync(path)
+  return files.length === 0 || (files.length === 1 && files[0] === '.git')
+}
+
+/**
+ * Create or overwrite folder root
+ *
+ * @param root
+ * @param overwrite
+ * @returns
+ */
+export function folderDirectory (root: string, overwrite = false) {
+  console.log(root)
+
+  if (overwrite) return emptyDirectory(root)
+
+  if (!fs.existsSync(root)) {
+    fs.mkdirSync(root, {recursive: true})
+  }
+}
+
+function emptyDirectory (dir: string) {
+  if (!fs.existsSync(dir)) return
+
+  for (const file of fs.readdirSync(dir)) {
+    if (file === '.git') continue
+
+    fs.rmSync(path.resolve(dir, file), {recursive: true, force: true})
+  }
+}
+
+export function getTemplateDirectory (path: string) {
+  const templateDir = resolve(fileURLToPath(import.meta.url), '../..', 'src/templates', path)
+  if (!fs.existsSync(templateDir)) throw new Error('No existe template')
+
+  return templateDir
+}
+
+export function isValidPackageName (projectName: string) {
+  return /^(?:@[a-z\d\-*~][a-z\d\-*._~]*\/)?[a-z\d\-~][a-z\d\-._~]*$/.test(projectName)
+}
+
+export function getValidPackageName (projectName: string) {
+  return projectName
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, '-')
+    .replace(/^[._]/, '')
+    .replace(/[^a-z\d\-~]+/g, '-')
+}
+
+export function setNamePackaging ({
+  directory,
+  packageName,
+  root,
+}: {
+  directory: string
+  packageName: string
+  root: string
+}) {
+  const packagePath = path.join(directory, `package.json`)
+  if (!fs.existsSync(packagePath)) return
+
+  const packageFile = JSON.parse(fs.readFileSync(packagePath, 'utf-8'))
+  packageFile.name = packageName
+
+  writeFile({file: 'package.json', content: JSON.stringify(packageFile, null, 2) + '\n', root, directory})
+}
+
+export function getPkgManager () {
+  const userAgent = process.env.npm_config_user_agent
+
+  if (!userAgent) return 'npm'
+
+  console.log(userAgent)
+
+  const pkgSpec = userAgent.split(' ')[0]
+  const pkgSpecArr = pkgSpec.split('/')
+
+  return pkgSpecArr[0]
+}
+
+export function writeFromFolder ({
+  directory,
+  ignoreFiles = [],
+  root,
+}: {
+  directory: string
+  ignoreFiles: Array<string>
+  root: string
+}) {
+  const filesRead = fs.readdirSync(directory)
+  const files = filesRead.filter(f => !ignoreFiles.includes(f))
+
+  for (const file of files) {
+    writeFile({file, directory, root})
+  }
+}
+
+export function writeFile ({
+  file,
+  content,
+  root,
+  directory,
+}: {
+  file: string
+  content?: string
+  root: string
+  directory: string
+}) {
+  const targetPath = path.join(root, file)
+
+  if (content) {
+    fs.writeFileSync(targetPath, content)
+  } else {
+    copy(path.join(directory, file), targetPath)
+  }
+}
+
+function copy (source: string, target: string) {
+  const stat = fs.statSync(source)
+
+  if (stat.isDirectory()) {
+    copyDirectory(source, target)
+  } else {
+    fs.copyFileSync(source, target)
+  }
+}
+
+function copyDirectory (sourceDirectory: string, targetDirectory: string) {
+  fs.mkdirSync(targetDirectory, {recursive: true})
+
+  for (const file of fs.readdirSync(sourceDirectory)) {
+    const srcFile = path.resolve(sourceDirectory, file)
+    const destFile = path.resolve(targetDirectory, file)
+    copy(srcFile, destFile)
+  }
+}
